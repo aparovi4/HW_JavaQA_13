285031874a4475a2f5ee565aa283e17c28b89135
Neto Aparovich <aparovich.gregory@gmail.com>
2023-03-14 06:28:42 +0300

    incomprehensible error

---
+++ src/main/java/Game/NotRegisteredException.java
@@ -0,0 +1,6 @@
+package Game;
+public class NotRegisteredException extends RuntimeException {
+    public NotRegisteredException (String playerName) {
+        super("Игрок с именем "+ playerName + " не найден");
+    }
+}
---
+++ src/test/java/Game/GameTest.java
@@ -0,0 +1,59 @@
+package Game;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+    @Test
+    public void firstPlayerIsStronger(){
+        Game game = new Game();
+        Player greg = new Player(1, "Greg", 10);
+        Player mark = new Player(2, "Mark", 15);
+
+        game.register(greg);
+        game.register(mark);
+        int actual = game.round("Mark","Greg");
+        int expected = 1;
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void secondPlayerIsStronger(){
+        Game game = new Game();
+        Player greg = new Player(1, "Greg", 10);
+        Player mark = new Player(2, "Mark", 15);
+
+        game.register(greg);
+        game.register(mark);
+        int actual = game.round("Greg", "Mark");
+        int expected = 2;
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void whenEqual(){
+        Game game = new Game();
+        Player greg = new Player(1, "Greg", 10);
+        Player mark = new Player(2, "Mark", 10);
+
+        game.register(greg);
+        game.register(mark);
+        int actual = game.round("Greg", "Mark");
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void whenFirstPlayerDoesNotExist(){
+        Game game = new Game();
+        Player greg = new Player(1, "Greg", 10);
+        Player mark = new Player(2, "Mark", 10);
+
+        game.register(greg);
+        game.register(mark);
+
+        Assertions.assertThrows(NotRegisteredException.class,
+                () -> game.round("Tom","Mark"));
+    }
+
+}
